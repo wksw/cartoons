05b0f9f1c8df03695de7637d5ba0d6f0a5afe445
wksw <2223721904@qq.com>
2018-07-02 01:26:06 +0800

    36 cartoons

---
+++ cartoon.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+ROOTDIR=$(cd $(dirname $0);pwd)
+NAME=${1:-'jushuowoshiwangdenver'}
+FROM=${2:-175440}
+
+rm -rf $ROOTDIR/$NAME
+mkdir -p $ROOTDIR/$NAME
+
+CHAPTERIMAGES='['
+
+while true
+do
+	html=$(curl -s https://www.36mh.com/manhua/$NAME/${FROM}.html)
+	chapterPath=$(echo $html |awk -F '<script>' '{print $3}'|awk -F '</script>' '{print $1}'  |awk -F ';' '{print $6}' |awk -F '=' '{print $2}' | sed s/[[:space:]]//g)
+	chapterImages=$(echo $html |awk -F '<script>' '{print $3}'|awk -F '</script>' '{print $1}' |awk -F ';' '{print $5}' |awk -F '=' '{print $2}' | sed s/[[:space:]]//g)
+	title=$(echo $html |awk -F '<script>' '{print $3}'|awk -F '</script>' '{print $1}' |awk -F 'pageTitle' '{print $2}'|awk -F ';' '{print $1}' |awk -F '=' '{print $2}')
+	FROM=$(echo $html |awk -F '<script>' '{print $3}'|awk -F '</script>' '{print $1}' |awk -F 'nextChapterData' '{print $2}' |awk -F ',' '{print $1}' |awk -F ':' '{print $2}' | sed s/[[:space:]]//g)
+	CHAPTERIMAGES="${CHAPTERIMAGES}{title:${title},chapterpath:${chapterPath},chapterImages:${chapterImages}},"
+	if [ "$FROM" = "null" ];then
+		break;
+	fi
+done
+
+CHAPTERIMAGES="${CHAPTERIMAGES}]"
+
+cat << EOF > $ROOTDIR/$NAME/index.html
+<html>
+<head>
+
+<style type="text/css">
+body {
+	text-align:cneter;
+}
+button {
+	width: 100px;
+	height: 50px;
+}
+</style>
+</head>
+
+<body>
+
+<div id="content" style="text-align:center;">
+</div>
+
+<script >
+
+var thisPage = 0
+var chapterImages = $CHAPTERIMAGES
+
+function nextChapter() {
+    thisPage++
+    if (thisPage > chapterImages.length) {
+        return
+    }
+    var html = show(thisPage)
+    document.getElementById("content").innerHTML=html;
+    window.scrollTo(0,0);
+    localStorage.setItem("chapter", thisPage)
+}
+
+function prevChapter() {
+    thisPage--
+    if (thisPage == 0) {
+        return
+    }
+    var html = show(thisPage)
+    document.getElementById("content").innerHTML=html;
+    window.scrollTo(0,0);
+    localStorage.setItem("chapter", thisPage)
+}
+
+
+function show(index) {
+    var html = '<button onclick="nextChapter()">next</button><button onclick="prevChapter()">preview</button><hr><h3>' + index + '(' + chapterImages.length + ')' + ' chapter ' +chapterImages[index].title+ '</h3>'
+    for (var i=0; i<chapterImages[index].chapterImages.length;i++) {
+        html += '<img src="' + 'https://img001.yayxcc.com/' + chapterImages[thisPage].chapterpath + chapterImages[thisPage].chapterImages[i] + '">'
+    }
+    html += '<hr><button onclick="nextChapter()">next</button><button onclick="prevChapter()">preview</button><h3>' + index + '(' + chapterImages.length + ')' + ' chapter ' +chapterImages[index].title+ '</h3>'
+    return html    
+}
+
+var thisPage = localStorage.getItem("chapter") || 0
+var html = show(thisPage)
+document.getElementById("content").innerHTML=html;
+</script> 
+
+</body>
+
+</html>
+
+EOF
---
+++ cartoons.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+ROOTDIR=$(cd $(dirname $0);pwd)
+
+html=$(curl -s https://www.36mh.com/list/lianzai/)
+totalpage=$(echo  $html | sed -r 's/.*<li class="last"><a href=(.+)data-page.*/\1/'|awk -F '"' '{print $2}' |awk -F '/' '{print $4}')
+echo "------------total $totalpage pages---------"
+for i in $(seq 1 $totalpage)
+do
+	echo "============page $i=========="
+	if [ $i -eq 1 ];then
+		i=""
+	fi
+	html=$(curl -s https://www.36mh.com/list/lianzai/$i)
+	pages=$(echo  $html |awk -F '<ul' '{print $16}' |awk -F '<img' '{for(i=1;i<NF;i++){print $i}}'| sed -r 's/.*href=(.+)title.*/\1/' |awk -F '"' '{print $2}')
+	for p in $pages
+	do
+		cartoon=$(curl -s $p)
+		name=$(echo $p |awk -F '/' '{print $(NF-1)}')
+		from=$(echo $cartoon | awk -F '<ul id="chapter-list-4"' '{print $2}'|awk -F '.html' '{print $1}'|awk -F '/' '{print $NF}')
+		echo $name $from
+		/bin/bash cartoon.sh $name $from &
+	done
+	wait
+done
